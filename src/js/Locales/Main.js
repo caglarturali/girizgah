c19b43e0061d0089d5159d0f72f7ed0834d05189
Caglar Turali <caglarturali@gmail.com>
2018-12-16 23:36:46 +0300

    Extract locale specific functionality into its own module

---
+++ src/js/Locales/Main.js
@@ -0,0 +1,23 @@
+// Locales -> Required by Main
+// --------------------------------------
+// Loads locale specific language.
+
+import { addLocaleData } from 'react-intl';
+import en from 'react-intl/locale-data/en';
+import tr from 'react-intl/locale-data/tr';
+import localeData from '../../locales/data.json';
+
+addLocaleData([...en, ...tr]);
+
+// Define user's language. Different browsers have the user locale defined
+// on different fields on the `navigator` object, so we make sure to account
+// for these different by checking all of them
+const language = (navigator.languages && navigator.languages[0]) || navigator.language || navigator.userLanguage;
+
+// Split locales with a region code
+const languageWithoutRegionCode = language.toLowerCase().split(/[_-]+/)[0];
+
+// Try full locale, try locale without region code, fallback to 'en'
+const messages = localeData[languageWithoutRegionCode] || localeData[language] || localeData.en;
+
+export { language, messages };
