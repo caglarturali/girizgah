c600d8dce3f7874e844f107bf53cd516aa040008
Caglar Turali <caglarturali@gmail.com>
2018-12-16 16:16:20 +0300

    Modularize themes

--- src/js/Reducers/SettingsReducer.js
+++ src/js/Reducers/SettingsReducer.js
@@ -1,6 +1,7 @@
 /* eslint { no-redeclare: 0 } */
 import * as Settings from '../Logic/Settings';
 import { setPageZoom } from '../Utils/Utils';
+import Glass from '../Components/Themes/Glass';
 
 export function addAdditionalSettings(state) {
   // Define our defaults
@@ -17,36 +18,12 @@ export function addAdditionalSettings(state) {
     page_zoom: 1.0,
     avatar_background_enabled: false,
     font_scale: 1.0,
-    // experimental_stars_enabled: false,
     staggered_animations_enabled: true,
     user_switcher_enabled: true,
+    // experimental_stars_enabled: false,
 
     // Default to glass
-    avatar_enabled: true,
-    avatar_size: '200px',
-    avatar_shape: 'circle',
-    date_enabled: true,
-    date_format: '<em>%A</em>, the <em>%o</em> of <em>%B</em>',
-    time_enabled: true,
-    time_format: '%H:%M',
-    hostname_enabled: true,
-    command_shutdown_enabled: true,
-    command_reboot_enabled: true,
-    command_hibernate_enabled: true,
-    command_sleep_enabled: true,
-    command_icons_enabled: true,
-    command_text_align: 'left',
-    style_command_background_color: 'hsla(0, 2%, 98%, 0.29)',
-    style_command_icon_color: 'hsl(182, 79%, 58%)',
-    style_command_text_color: 'hsla(0, 0%, 90%, 0.9)',
-    style_login_border_color: 'hsla(0, 0%, 0%, 0)',
-    style_login_border_enabled: false,
-    style_login_button_color: 'hsla(0, 0%, 0%, 0.45)',
-    style_login_gradient_top_color: 'hsla(0, 0%, 100%, 0)',
-    style_login_gradient_bottom_color: 'hsla(0, 0%, 100%, 0.32)',
-    style_login_username_color: 'hsla(0, 100%, 100%, 1)',
-    window_border_radius: '4px',
-    window_font_size: '1em'
+    ...Glass
   };
 
   let settings = {};
